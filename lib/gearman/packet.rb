7871f214bcb27988b0dde39897a452dacfc6ab94
Brian Cobb <brian@cramerdev.com>
2012-04-30 08:34:27 -0500

    NAMES

--- lib/gearman/packet.rb
+++ lib/gearman/packet.rb
@@ -1,18 +1,19 @@
 module Gearman
   class Packet
 
-    PACKING = 'a4NN'
+    HEADER_PACKING = 'a4NN'
+    NULL_BYTE = "\0"
 
     def self.load(socket)
-      magic, type, body_length = socket.read(12).unpack(PACKING)
-      arguments = socket.read(body_length).split("\0")
+      magic, type, body_length = socket.read(12).unpack(HEADER_PACKING)
+      arguments = socket.read(body_length).split(NULL_BYTE)
 
       new(magic, type, arguments)
     end
 
     def self.dump(packet)
-      body = packet[:arguments].join("\0")
-      header = [packet[:magic], packet[:type], body.length].pack(PACKING)
+      body = packet[:arguments].join(NULL_BYTE)
+      header = [packet[:magic], packet[:type], body.length].pack(HEADER_PACKING)
 
       header + body
     end
