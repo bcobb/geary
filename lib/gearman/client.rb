45b05d62ed77c9a353f1840674bafccb172181a2
Brian <bcobb@uwalumni.com>
2012-05-01 07:59:09 -0500

    Move auto-generated crap to Request-level.

--- lib/gearman/client.rb
+++ lib/gearman/client.rb
@@ -1,46 +1,9 @@
 module Gearman
   class Client
 
-    REQUESTS = {
-      :echo => 16,
-      :submit_job => 7,
-      :submit_job_high => 21,
-      :submit_job_low => 33,
-      :submit_job_bg => 18,
-      :submit_job_high_bg => 32,
-      :submit_job_low_bg => 34,
-      :submit_job_epoch => 36,
-      :get_status => 15,
-      :set_option => 26
-    }
-
     def initialize(server)
       @server = server
     end
-
-    def self.can(name, args = [])
-      return if args.empty?
-
-      type = REQUESTS.fetch(name) { name }
-      class_eval %{
-        def #{name}(#{args.join(", ")})
-          request = Request.new(#{type}, #{args.join(", ")})
-          @server.send(request)
-        end
-      }
-    end
-    private_class_method :can
-
-    can :echo, [:data]
-    can :submit_job, [:job, :job_id, :data]
-    can :submit_job_high, [:job, :job_id, :data]
-    can :submit_job_low, [:job, :job_id, :data]
-    can :submit_job_bg, [:job, :job_id, :data]
-    can :submit_job_high_bg, [:job, :job_id, :data]
-    can :submit_job_low_bg, [:job, :job_id, :data]
-    can :submit_job_epoch, [:job, :job_id, :epoch_time, :data]
-    can :get_status, [:job_handle]
-    can :set_option, [:option_name]
 
   end
 end
--- lib/gearman/request.rb
+++ lib/gearman/request.rb
@@ -3,6 +3,27 @@ module Gearman
 
     MAGIC = "\0REQ"
 
+    def self.type(map, *arguments)
+      map.each do |method, type|
+        instance_eval %{
+          def #{method}(#{arguments.join(", ")})
+            Request.new(#{type}, #{arguments.join(", ")})
+          end
+        }
+      end
+    end
+
+    type :echo => 16, :data
+    type :submit_job => 7, :function_name, :unique_id, :data
+    type :submit_job_high => 21, :function_name, :unique_id, :data
+    type :submit_job_low => 33, :function_name, :unique_id, :data
+    type :submit_job_bg => 18, :function_name, :unique_id, :data
+    type :submit_job_high_bg => 32, :function_name, :unique_id, :data
+    type :submit_job_low_bg => 34, :function_name, :unique_id, :data
+    type :submit_job_epoch => 36, :function_name, :unique_id, :data
+    type :get_status => 36, :job_handle
+    type :option => 26, :option_name
+
     def initialize(type, *arguments)
       @packet = Packet.new MAGIC, type, arguments
     end
