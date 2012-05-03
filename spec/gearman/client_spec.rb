74e45de96f6c9575d2523db93dd8e92137c1f872
Brian Cobb <brian@cramerdev.com>
2012-05-03 17:25:13 -0500

    WWOOOO

--- spec/gearman/client_spec.rb
+++ spec/gearman/client_spec.rb
@@ -2,4 +2,20 @@ require 'gearman'
 
 describe Gearman::Client do
 
+  # XXX: Gearman.configure { }
+  subject { Gearman::Client.new }
+
+  # user: client + server(s)
+  # client -> tell server to echo data
+  #           => grab connection from connection pool
+  #           => send request over connection, read response into a generic
+  #           response container
+  #           => convert response to the expected type
+  #           => if conversion fails, read as error
+  #           => if can't read as error, raise
+  it 'can echo data' do
+    request = stub(:echo_req)
+    subject.echo 'data', request
+  end
+
 end
