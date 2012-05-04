bce1203a0fff5bc4bbd4810eb42f5b713ba70ec2
Brian <bcobb@uwalumni.com>
2012-05-04 14:15:34 -0500

    Sketch out using a connection to gearman with a connection pool

--- lib/gearman.rb
+++ lib/gearman.rb
@@ -1,6 +1,28 @@
-require 'socket'
-
 require 'gearman/packet'
 require 'gearman/stream'
 require 'gearman/request'
 require 'gearman/client'
+
+module Gearman
+
+  def self.connect(&block)
+    if connection_pool
+      connection_pool.with_connection do |server|
+        begin
+          block.call(server)
+        ensure
+          server.close_connection
+        end
+      end
+    end
+  end
+
+  def self.configuration
+    @configuration ||= Gearman::Configuration.new
+  end
+
+  def self.configure
+    yield configuration if block_given?
+  end
+
+end
--- lib/gearman/client.rb
+++ lib/gearman/client.rb
@@ -1,8 +1,12 @@
 module Gearman
   class Client
 
-    def initialize(server)
-      @server = server
+    def echo(data, request_container = Request::EchoReq,
+             response_reader = Response::EchoRes)
+      Gearman.connect do |server|
+        server.request(request_container.new(data))
+        response_reader.read_response(server)
+      end
     end
 
   end
